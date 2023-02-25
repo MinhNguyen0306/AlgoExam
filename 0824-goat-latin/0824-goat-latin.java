444d4380855f6c7926af80a3245d487b7ff64732
MinhLe <66161576+MinhNguyen0306@users.noreply.github.com>
2023-02-25 10:14:53 +0700

    Time: 2 ms (89.78%), Space: 41.1 MB (62.18%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,20 +1,25 @@
 class Solution {
     public String toGoatLatin(String sentence) {
         StringBuilder result = new StringBuilder();
-        String[] arr = sentence.split(" ");
+        String[] spl = sentence.split(" ");
         String conStr = "maa";
-        int sSize = arr.length;
+        int sSize = spl.length;
         for(int i = 0; i < sSize; i++) {
-            if(isVowel(arr[i].charAt(0))) 
-                result.append(arr[i] + conStr + " ");
-            else 
-                result.append(arr[i].substring(1) + arr[i].charAt(0) + conStr + " ");
+            StringBuilder arr = new StringBuilder(spl[i]); 
+            if(!isVowel(arr.charAt(0))) {
+                char temp = arr.charAt(0);
+                arr.deleteCharAt(0);
+                arr.append(temp);
+            }
+            arr.append(conStr + " ");
+            result.append(arr);
+                
             conStr += 'a';
         }
         return result.toString().trim();
     }
     
-    boolean isVowel(char c) {
-        return (c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u'|| c=='A'|| c=='E'|| c=='I'|| c=='O'|| c=='U');
+    public boolean isVowel(char c) {
+        return c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u'|| c=='A'|| c=='E'|| c=='I'|| c=='O'|| c=='U';
     }
 }
