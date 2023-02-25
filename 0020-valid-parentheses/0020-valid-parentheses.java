5f654a3cbc28fe55c332566416ab22532dccb88f
MinhLe <66161576+MinhNguyen0306@users.noreply.github.com>
2023-02-25 21:26:33 +0700

    Time: 2 ms (56.65%), Space: 40.4 MB (44.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if(s.length() <= 1) return false;
+        for(char c : s.toCharArray()) {
+            if(c == '[') {
+                stack.push(']');
+            } else if(c == '(') {
+                stack.push(')');
+            } else if(c == '{') {
+                stack.push('}');
+            } else {
+                if(stack.empty() || c != stack.pop() ) return false;
+            }
+        }
+        return stack.empty();
+    }
+}
