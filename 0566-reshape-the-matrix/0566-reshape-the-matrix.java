4e318665aa9b3dfa93d5298f12529fca18d4ec69
MinhLe <66161576+MinhNguyen0306@users.noreply.github.com>
2023-02-26 12:53:23 +0700

    Time: 0 ms (100.00%), Space: 43.5 MB (41.21%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -8,23 +8,23 @@ class Solution {
         int[][] newMat = new int[r][c];
         
         // Optimize
-        // for(int i = 0; i < r * c; i++) {
-        //     newMat[i/c][i%c] = mat[i/n][i%n];
-        // }
+        for(int i = 0; i < r * c; i++) {
+            newMat[i/c][i%c] = mat[i/n][i%n];
+        }
         
         // Brute Force
-        int row = 0;
-        int col = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                newMat[row][col] = mat[i][j];
-                col++;
-                if(col == c) {
-                    col = 0;
-                    row++;
-                }
-            }
-        }
+        // int row = 0;
+        // int col = 0;
+        // for(int i = 0; i < m; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         newMat[row][col] = mat[i][j];
+        //         col++;
+        //         if(col == c) {
+        //             col = 0;
+        //             row++;
+        //         }
+        //     }
+        // }
         
         return newMat;
     }
