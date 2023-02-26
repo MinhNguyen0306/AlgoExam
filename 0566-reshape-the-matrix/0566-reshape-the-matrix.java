99c54a60ab82b594c28814ae24385c086474b674
MinhLe <66161576+MinhNguyen0306@users.noreply.github.com>
2023-02-26 12:52:09 +0700

    Time: 0 ms (100.00%), Space: 43.5 MB (31.06%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        if(r * c != m * n) return mat;
+        
+        int[][] newMat = new int[r][c];
+        
+        // Optimize
+        // for(int i = 0; i < r * c; i++) {
+        //     newMat[i/c][i%c] = mat[i/n][i%n];
+        // }
+        
+        // Brute Force
+        int row = 0;
+        int col = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                newMat[row][col] = mat[i][j];
+                col++;
+                if(col == c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        
+        return newMat;
+    }
+}
