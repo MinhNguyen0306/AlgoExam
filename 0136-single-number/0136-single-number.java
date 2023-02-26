5b83aec5d4e2fb56b93ba7cb8f512350cf065e9c
MinhLe <66161576+MinhNguyen0306@users.noreply.github.com>
2023-02-26 23:15:46 +0700

    Time: 1 ms (100.00%), Space: 42.6 MB (44.55%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        List<Integer> vector = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++) {
-            if(!vector.contains(nums[i])) {
-                vector.add(nums[i]);
-            } else {
-                vector.remove(vector.indexOf(nums[i]));
-            }
+        int result = 0;
+
+        for (int num : nums) {
+            result = result ^ num;
         }
-        System.out.print(vector);
-        return vector.get(0);
+        return result;
     }
 }
