8fa150d08df0431e285cd2da380ee729ee5fba8a
MinhLe <66161576+MinhNguyen0306@users.noreply.github.com>
2023-03-14 09:08:49 +0700

    Time: 19 ms (23.95%), Space: 42 MB (49.44%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,8 +3,11 @@ class Solution {
         Arrays.sort(nums);
         int sum = nums[0] + nums[1] + nums[nums.length - 1];
         for(int i = 0; i < nums.length - 2; i++) {
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int j = i + 1, k = nums.length - 1;
             while(j < k) {
+                // while(j < k && nums[j+1] == nums[j]) j++;
+                // while(j < k && nums[k-1] == nums[k]) k--;
                 int cur_sum = nums[i] + nums[j] + nums[k];
                 if(cur_sum > target) k--;
                 else j++;
